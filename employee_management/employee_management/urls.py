c83517b6620d522a3eb838ec1fa757be17b2a042
Ahammed Faisal K <ahammedfaisal70@gmail.com>
2025-10-07 07:02:14 +0530

    Updated urls

--- employee_management/employee_management/urls.py
+++ employee_management/employee_management/urls.py
@@ -1,8 +1,25 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from employees.api.v1 import urls as employee_api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/employees/', include(employee_api_urls, namespace="api_v1_employees")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+
+# Debug toolbar URLs (only in DEBUG mode)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
